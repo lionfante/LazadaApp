32c4e82dea04063e253e1c434d6a1adb6a5af3d6
Thien Phu <lethienphu.1404@gmail.com>
2017-03-29 16:31:19 +0700

    second commit

---
+++ app/src/main/java/thienphu/lazadaapp/UIView/ManHinhChao/ManHinhChaoActivity.java
@@ -0,0 +1,19 @@
+package thienphu.lazadaapp.UIView.ManHinhChao;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import thienphu.lazadaapp.R;
+
+/**
+ * Created by lionf_000 on 29-Mar-17.
+ */
+
+public class ManHinhChaoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinhchao_layout);
+    }
+}
